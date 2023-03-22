7fe3f01fe0c6deb7c75c308a2c1f25bd56571ab9
Elizabeth Reisher <80726772+ereisher@users.noreply.github.com>
2023-03-22 15:11:13 -0500

    Update get_sample_name_dic.sh

--- Pre-Process/get_sample_name_dic.sh
+++ Pre-Process/get_sample_name_dic.sh
@@ -7,7 +7,7 @@ acc_list_file='SRR_Acc_List.txt'
 # Counter
 n=0
 
-# Read line by line from the txt file to download the raw reads 
+# Read line by line from the text file to download the raw reads 
 while read line; do
 	echo "$line" | tr '\n' ' '
 	n=$((n+1))
