94ccd92de0bd6272ec813b65323f0cdf52d980f3
Elizabeth Reisher <80726772+ereisher@users.noreply.github.com>
2023-03-22 15:09:28 -0500

    Update header_clean.py

--- LEfSe/header_clean.py
+++ LEfSe/header_clean.py
@@ -1,19 +1,22 @@
-test = open("rel_level_6.txt", "r")
+# Open the input file
+input_file = open("rel_level_6.txt", "r")
+
+# Open output file
 file = open("clean_rel_level_6.txt", "w")
 
-test = test.readlines()
+# Read the input file
+input_file = input_file.readlines()
 
-for line in test:
+# Iterate and clean input 
+for line in input_file:
     if line.startswith("d_"):
         line = line.split(";")
-        #print(holder)
         line = line[5:]
-        #print(line)
         line = " ".join(line)
         file.write(line)
     else:
         file.write(line)
 
-#print(test)
+# Close the file
 file.close()
         
