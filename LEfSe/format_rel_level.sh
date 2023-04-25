80226bdb485e9d6ae19b9ee32b579f10970175cf
Elizabeth Reisher <80726772+ereisher@users.noreply.github.com>
2023-04-25 10:39:19 -0500

    Update format_rel_level.sh

--- LEfSe/format_rel_level.sh
+++ LEfSe/format_rel_level.sh
@@ -7,16 +7,15 @@ module load lefse/1.0
 # Get the phyla_table_rel_level files
 cp ../exports/phyla_table_rel_level_6/phyla_table_rel_level_6.tsv .
 
-# Generate the sample list with conditions
+# Generate the sample list with differences to study
 cut ../metadata.tsv -f 1,2 > sample_list_type.tsv
 
-# Clean Eukaryotes and unassigned
+# Remove Eukaryotes and unassigned
 grep -v "Eukar" phyla_table_rel_level_6.tsv >tmp
 grep -v "Unassigned" tmp >phyla_table_rel_level_6.tsv 
 
 # Get the correct format to run LEfSe
 python rel_format.py -i sample_list_type.tsv -r phyla_table_rel_level_6.tsv -o rel_level_6.tsv
-
 sed 's/\t/\t/g' rel_level_6.tsv > rel_level_6.txt
 
 # Clean the header to match paper
